faf8c653a1d9f21d663ee766e16fa80c9a03efdc
MischaGe <gemsa.michael@gmail.com>
2022-09-19 17:25:21 +0200

    Initial commit

---
+++ src/Loop1.java
@@ -0,0 +1,30 @@
+public class Loop1 implements Runnable {
+    private String myName;
+
+    public Loop1 (String name) {
+        myName = name;
+    }
+
+    public void run() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(myName + " (" + i + ") ");
+        }
+    }
+
+    public static void main (String[] args) {
+
+        // Aufgabe 2.2
+
+        Loop1 l1 = new Loop1("Thread 1");
+        Loop1 l2 = new Loop1("Thread 2");
+        Loop1 l3 = new Loop1("Thread 3");
+
+        Thread t1 = new Thread(l1);
+        Thread t2 = new Thread(l2);
+        Thread t3 = new Thread(l3);
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
---
+++ src/Loop2.java
@@ -0,0 +1,22 @@
+public class Loop2 {
+
+    public static void hauRaus(String myName) {
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println(myName + " (" + i + ") ");
+        }
+    }
+
+    public static void main (String[] args) {
+
+        // Aufgabe 2.3
+
+
+        Thread t1 = new Thread(() -> hauRaus("Thread 1"));
+        Thread t2 = new Thread(() -> hauRaus("Thread 2"));
+        Thread t3 = new Thread(() -> hauRaus("Thread 3"));
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
