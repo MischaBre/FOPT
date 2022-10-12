74f3a8d13da496a4a766eb70b48f1c553bddd742
MischaGe <gemsa.michael@gmail.com>
2022-10-12 12:33:18 +0300

    added own ReentrantLock

---
+++ src/zwoelfEins/ReentrantLock.java
@@ -0,0 +1,36 @@
+package zwoelfEins;
+
+public class ReentrantLock {
+
+    private int counter;
+    private Thread currentThread;
+
+    public ReentrantLock() {
+        counter = 0;
+    }
+
+    public synchronized void lock() {
+        while (counter > 0 && Thread.currentThread() != currentThread) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if (Thread.currentThread() != currentThread) {
+            currentThread = Thread.currentThread();
+        }
+        counter++;
+    }
+
+    public synchronized void unlock() {
+        if (Thread.currentThread() != currentThread) {
+            throw new IllegalAccessError("Falscher Thread");
+        }
+        counter--;
+        if (counter == 0) {
+            currentThread = null;
+            notify();
+        }
+    }
+}
